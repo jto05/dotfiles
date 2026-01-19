e594bc1287f12047addc37173427b75a9b93b1a9
jto05 <julian.to2023@gmail.com>
2026-01-18 17:56:49 -0800

    (feat:wezterm) Updated cursor style

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,11 +3,16 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- STYLE
-config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
+-- font and color scheme
 config.font_size = 14
 config.color_scheme = 'catppuccin-mocha'
+
+-- window
+config.enable_tab_bar = false
+config.window_decorations = 'RESIZE'
+
+-- cursor
+config.default_cursor_style = 'SteadyBlock'
 
 -- Finally, return the configuration to wezterm:
 return config
