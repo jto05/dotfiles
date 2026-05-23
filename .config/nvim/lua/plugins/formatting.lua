b95ea97191f1ea5ffa947d23f5f68dcddfa5a1ab
jto05 <julian.to2023@gmail.com>
2026-05-23 16:59:50 -0700

    (feat:nvim) add nvim-ufo for treesitter-based code folding

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -20,4 +20,23 @@ return {
       })
     end
   },
+
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+    },
+
+    config = function()
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      require('ufo').setup({
+          provider_selector = function(bufnr, filetype, buftype)
+              return {'treesitter', 'indent'}
+          end
+      })
+    end
+  }
+
 }
