852762df6c5219b45015a57d0d727982f6015c06
jto05 <julian.to2023@gmail.com>
2026-05-23 16:59:37 -0700

    (refactor:nvim) restructure formatting.lua as multi-plugin return

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,21 +1,23 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require("conform")
 
-    conform.setup({
-      formatteres_by_ft = {
-        lua = { "stylua" },
-        go = { "gofumpt" },
-        python = { "ruff" },
-      },
+      conform.setup({
+        formatteres_by_ft = {
+          lua = { "stylua" },
+          go = { "gofumpt" },
+          python = { "ruff" },
+        },
 
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      }
-    })
-  end
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        }
+      })
+    end
+  },
 }
