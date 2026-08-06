e93c487c2ca2f4d88e4ada003bd15f4b9507d1a0
jto05 <julian.to2023@gmail.com>
2026-08-05 19:40:45 -0700

    updated dms, changed wezterm opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- font and color scheme
-config.font = wezterm.font 'CaskaydiaMonoNerdFont'
-config.font_size = 14
+config.font = wezterm.font("CaskaydiaMono Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font_size = 15
 config.color_scheme = 'Gruvbox Dark (Gogh)'
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.9
+config.initial_cols = 67
+config.initial_rows = 50
 
 -- window
 config.enable_tab_bar = false
